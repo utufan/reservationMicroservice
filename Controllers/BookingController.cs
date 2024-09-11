754cac87683c4a0572b8d5ca8a8582a7629ba1a4
utufan <h.tufanusta@gmail.com>
2024-09-11 11:32:27 +0200

    wrong booking controller changed

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -1,6 +1,6 @@
-using dfdsMicroserviceProject.Models;
-using dfdsMicroserviceProject.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using dfdsMicroserviceProject.Services.Interfaces;
+using dfdsMicroserviceProject.Dtos;
 
 namespace dfdsMicroserviceProject.Controllers
 {
@@ -8,160 +8,62 @@ namespace dfdsMicroserviceProject.Controllers
     [ApiController]
     public class BookingController : ControllerBase
     {
-        private readonly IBookingRepository _bookingRepository;
-        private readonly IPassengerRepository _passengerRepository;
+        private readonly IBookingService _bookingService;
 
-        public BookingController(IBookingRepository bookingRepository, IPassengerRepository passengerRepository)
+        public BookingController(IBookingService bookingService)
         {
-            _bookingRepository = bookingRepository;
-            _passengerRepository = passengerRepository;
+            _bookingService = bookingService;
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<BookingDto>> GetBookings()
         {
-            var bookings = _bookingRepository.GetAllBookings();
-            var bookingDtos = MapToBookingDtos(bookings);
+            var bookingDtos = _bookingService.GetAllBookings();
             return Ok(bookingDtos);
         }
 
         [HttpGet("{id}")]
         public ActionResult<BookingDto> GetBooking(int id)
         {
-            var booking = _bookingRepository.GetBookingById(id);
-
-            if (booking == null)
+            var bookingDto = _bookingService.GetBookingById(id);
+            if (bookingDto == null)
             {
                 return NotFound();
             }
-
-            var bookingDto = MapToBookingDtos(new List<Booking> { booking }).FirstOrDefault();
             return Ok(bookingDto);
         }
 
-
         [HttpPost]
-        public ActionResult<Booking> PostBooking([FromBody] BookingRequest request)
+        public ActionResult<BookingDto> PostBooking([FromBody] BookingRequest request)
         {
-            var booking = new Booking();
-
-            foreach (var bp in request.BookingPassengers)
+            var bookingDto = _bookingService.CreateBooking(request);
+            if (bookingDto == null)
             {
-                if (bp.PassengerId.HasValue)
-                {
-                    var passenger = _passengerRepository.GetPassengerById(bp.PassengerId.Value);
-                    if (passenger == null)
-                    {
-                        return NotFound($"Passenger with ID {bp.PassengerId} not found");
-                    }
-                    booking.BookingPassengers.Add(new BookingPassenger { PassengerId = bp.PassengerId.Value, Passenger = passenger });
-                }
-                else if (bp.NewPassenger != null)
-                {
-                    var newPassenger = new Passenger
-                    {
-                        FirstName = bp.NewPassenger.FirstName,
-                        LastName = bp.NewPassenger.LastName,
-                        PassportNumber = bp.NewPassenger.PassportNumber,
-                        Nationality = bp.NewPassenger.Nationality
-                    };
-                    _passengerRepository.AddPassenger(newPassenger);
-                    booking.BookingPassengers.Add(new BookingPassenger { PassengerId = newPassenger.Id, Passenger = newPassenger });
-                }
+                return BadRequest("Failed to create booking.");
             }
-
-            _bookingRepository.AddBooking(booking);
-
-            return CreatedAtAction(nameof(GetBooking), new { id = booking.Id }, booking);
+            return CreatedAtAction(nameof(GetBooking), new { id = bookingDto.Id }, bookingDto);
         }
 
         [HttpPut("{id}")]
         public IActionResult PutBooking(int id, [FromBody] BookingRequest request)
         {
-            var booking = _bookingRepository.GetBookingById(id);
-            if (booking == null)
+            var result = _bookingService.UpdateBooking(id, request);
+            if (!result)
             {
                 return NotFound();
             }
-
-            booking.BookingPassengers.Clear();
-            foreach (var bp in request.BookingPassengers)
-            {
-                if (bp.PassengerId.HasValue)
-                {
-                    var passenger = _passengerRepository.GetPassengerById(bp.PassengerId.Value);
-                    if (passenger == null)
-                    {
-                        return NotFound($"Passenger with ID {bp.PassengerId} not found");
-                    }
-                    booking.BookingPassengers.Add(new BookingPassenger { PassengerId = bp.PassengerId.Value, Passenger = passenger });
-                }
-                else if (bp.NewPassenger != null)
-                {
-                    var newPassenger = new Passenger
-                    {
-                        FirstName = bp.NewPassenger.FirstName,
-                        LastName = bp.NewPassenger.LastName,
-                        PassportNumber = bp.NewPassenger.PassportNumber,
-                        Nationality = bp.NewPassenger.Nationality
-                    };
-                    _passengerRepository.AddPassenger(newPassenger);
-                    booking.BookingPassengers.Add(new BookingPassenger { PassengerId = newPassenger.Id, Passenger = newPassenger });
-                }
-            }
-
-            _bookingRepository.UpdateBooking(booking);
             return NoContent();
         }
 
         [HttpDelete("{id}")]
         public IActionResult DeleteBooking(int id)
         {
-            _bookingRepository.DeleteBooking(id);
+            var result = _bookingService.DeleteBooking(id);
+            if (!result)
+            {
+                return NotFound();
+            }
             return NoContent();
         }
-
-        private List<BookingDto> MapToBookingDtos(IEnumerable<Booking> bookings)
-        {
-            return bookings.Select(booking => new BookingDto
-            {
-                Id = booking.Id,
-                Passengers = booking.BookingPassengers.Select(bp => new PassengerDetailsDto
-                {
-                    Id = bp.Passenger.Id,
-                    FirstName = bp.Passenger.FirstName,
-                    LastName = bp.Passenger.LastName,
-                    PassportNumber = bp.Passenger.PassportNumber,
-                    Nationality = bp.Passenger.Nationality
-                }).ToList()
-            }).ToList();
-        }
     }
-
-    public class BookingRequest
-    {
-        public required List<BookingPassengerRequest> BookingPassengers { get; set; }
-    }
-
-    public class BookingPassengerRequest
-    {
-        public int? PassengerId { get; set; }
-        public Passenger? NewPassenger { get; set; }
-    }
-
-}
-
-public class BookingDto
-{
-    public int Id { get; set; }
-    public List<PassengerDetailsDto> Passengers { get; set; } = new();
-}
-
-public class PassengerDetailsDto
-{
-    public int Id { get; set; }
-    public required string FirstName { get; set; }
-    public required string LastName { get; set; }
-    public required string PassportNumber { get; set; }
-    public required string Nationality { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -21,9 +21,9 @@ builder.Services.AddDbContext<ReservationContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IPassengerRepository, PassengerRepository>();
-//builder.Services.AddScoped<IPassengerService, PassengerService>();
+builder.Services.AddScoped<IPassengerService, PassengerService>();
 builder.Services.AddScoped<IBookingRepository, BookingRepository>();
-//builder.Services.AddScoped<IBookingService, BookingService>();
+builder.Services.AddScoped<IBookingService, BookingService>();
 
 
 builder.Services.AddEndpointsApiExplorer();
